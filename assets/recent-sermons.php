b0706b78aedb9d5873a1cb7010141f1bd868b65f
breonw <breon_williams@unc.edu>
2016-12-03 14:11:06 -0500

    modify shortcode

--- assets/recent-sermons.php
+++ assets/recent-sermons.php
@@ -95,74 +95,133 @@ function sermon_datatables( $atts ) {
 
 
 
-add_shortcode( 'sermon_tables', 'sermon_tables' );
-function sermon_tables( $atts ) {
-    ob_start();
-    // define attributes and their defaults
-    extract( shortcode_atts( array (
-        'posts' => -1,
-        'category' => '',
-        'ptype' => 'sermons',
-        'sermon_category' => '',
-    ), $atts ) );
+
+/*sermon posts end*/
 
 
-    // define query parameters based on attributes
-    $options = array(
-        'posts_per_page' => $posts,
-        'post_type' => $ptype,
-        'category_name' => $category,
-        'sermon_category' => $sermon_category,
-    );
-    $query = new WP_Query( $options );
-    // run the loop based on the query
-    if ( $query->have_posts() ) { ?>
+if ( ! function_exists('sermon_tables') ) {
+    function sermon_tables( $atts ){
+        wp_enqueue_script( 'lity-js' );
 
-        <ul class="respList-wrap">
+        $atts = shortcode_atts( array(
+            'ptype' => 'sermons',
+            'per_page'  =>      1,
+            'order'     =>  'DESC',
+            'orderby'   =>  'date',
+            'category' => '',
+            'class' => '',
+            'column' => '',
+        ), $atts );
+
+        $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
+
+        $class = $atts['class'];
+
+        $args = array(
+            'post_type'    =>  $atts["ptype"],
+            'posts_per_page'    =>  $atts["per_page"],
+            'order'             =>  $atts["order"],
+            'orderby'           =>  $atts["orderby"],
+            'paged'             =>  $paged,
+            'category_name' => $category
+        );
+
+        $query = new WP_Query($args);
+
+        $output .= '<ul class="respList-wrap">';
+
+        if($query->have_posts()) : $output;
+
+            while ($query->have_posts()) : $query->the_post();
+
+                $output .= '<li id="post-' . get_the_ID() . '" class="resplist-item ' . implode(' ', get_post_class()) . '">';
+                $output .= '<div class="row">';
+                $output .= '<div class="col-md-6">';
+                $output .= '<div class="row">';
+                $output .= '<div class="col-sm-6">';
+
+                $output .= '<h3 class="post-title"><span><a href="' . get_permalink() . '" title="' . the_title('','',false) . '">' . the_title('','',false) . '</a></span></h3>';
+                $output .= '</div>';
+                $output .= '<div class="col-sm-6">';
+                if ( get_field('scripture' )) {
+                    $output .= get_field('scripture');
+                } else {
+                }
+                $output .= '</div>';
+                $output .= '</div>';
+                $output .= '</div>';
+
+                $output .= '<div class="col-md-6">';
+                $output .= '<div class="row">';
+                $output .= '<div class="col-md-4 col-sm-6">';
+
+                if ( get_field('speaker' )) {
+                    $output .= get_field('speaker');
+                } else {
+                }
+
+                $output .= '</div>';
+                $output .= '<div class="col-md-8 col-sm-6">';
+                $output .= '<ul class="list-inline">';
+
+                if ( get_field('video' )) {
+                    $output .= '<li><a href="' . get_field('video') . '" class="btn btn-bordered btn-primary btn-sm marginbot-15" data-lity>';
+                    $output .= 'Video <i class="fa fa-video-camera" aria-hidden="true"></i> ';
+                    $output .= '</a></li>';
+
+                } else {
+                }
+
+                if ( get_field('audio' )) {
+                    $output .= '<li><a href="' . get_field('audio') . '" class="btn btn-bordered btn-primary btn-sm marginbot-15" data-lity>';
+                    $output .= 'Audio <i class="fa fa-headphones" aria-hidden="true"></i> ';
+                    $output .= '</a></li>';
+
+                } else {
+                }
+                if ( get_field('document' )) {
+                    $output .= '<li><a href="' . get_field('document') . '" class="btn btn-bordered btn-primary btn-sm marginbot-15" data-lity>';
+                    $output .= 'Doc <i class="fa fa-file-o" aria-hidden="true"></i> ';
+                    $output .= '</a></li>';
+
+                } else {
+                }
+
+                $output .= '</ul>';
+                $output .= '</div>';
+                $output .= '</div>';
+                $output .= '</div>';
+                $output .= '</div>';
+                $output .= '</li>';
 
 
-            <?php while ( $query->have_posts() ) : $query->the_post(); ?>
-                <li class="resplist-item">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="row">
-                                <div class="col-sm-6"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php printf(__('%s', 'heels'), the_title_attribute('echo=0')); ?>"><?php the_title(); ?></a></div>
-                                <div class="col-sm-6">
-                                    <?php if( get_field('scripture') ): ?>
-                                        <p><em><?php the_field('scripture'); ?></em></p>
-                                    <?php endif; ?>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <?php if( get_field('speaker') ): ?>
-                                        <p><?php the_field('speaker'); ?></p>
-                                    <?php endif; ?>
-                                </div>
-                                <div class="col-sm-6">
-                                    <?php if( get_field('video') ): ?>
-                                        <a href="<?php the_field('video'); ?>" class="btn btn-bordered btn-primary btn-sm marginbot-15" data-lity>Video <i class="fa fa-video-camera" aria-hidden="true"></i> </a>
-                                    <?php endif; ?>
-                                    <?php if( get_field('audio') ): ?>
-                                        <a href="<?php the_field('audio'); ?>" data-lity class="btn btn-bordered btn-primary btn-sm marginbot-15">Audio <i class="fa fa-headphones" aria-hidden="true"></i></a>
-                                    <?php endif; ?>
-                                    <?php if( get_field('document') ): ?>
-                                        <a href="<?php the_field('document'); ?>" class="btn btn-bordered btn-primary btn-sm marginbot-15">Doc <i class="fa fa-file-o" aria-hidden="true"></i></a>
-                                    <?php endif; ?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-            <?php endwhile;
-            wp_reset_postdata(); ?>
 
-        </ul>
+            endwhile;global $wp_query;
+            $output .= '</div>';
 
-        <?php $myvariable = ob_get_clean();
-        return $myvariable;
+            $args_pagi = array(
+                'base' => add_query_arg( 'paged', '%#%' ),
+                'total' => $query->max_num_pages,
+                'current' => $paged
+            );
+            $output .= '<div class="clearfix"></div>';
+
+            $output .= '<div class="post-nav col-md-12">';
+            $output .= paginate_links( $args_pagi);
+
+            //    $output .= '<div class="next-page">' . get_next_posts_link( "Older Entries »", 3 ) . '</div>';
+
+            $output .= '</div>';
+
+        else:
+
+            $output .= '<p>Sorry, there are no posts to display</p>';
+
+        endif;
+        wp_reset_postdata();
+
+        return $output;
     }
 }
-/*sermon posts end*/
+
+add_shortcode('sermon_tables', 'sermon_tables');
