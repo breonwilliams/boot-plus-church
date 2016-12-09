1eda26f0ef687fae0363df4925e703da0394fbe4
breonw <breon_williams@unc.edu>
2016-12-09 13:21:53 -0500

    modif menu

--- assets/menu-shortcode.php
+++ assets/menu-shortcode.php
@@ -4,9 +4,9 @@
  */
 
 register_nav_menus(array(
-    'custom' => __('Custom Menu', 'bootstrap-basic'),
-    'popOut_primary' => __('Primary Pop-Out Menu', 'bootstrap-basic'),
-    'popOut_secondary' => __('Secondary Pop-Out Menu', 'bootstrap-basic'),
+    'custom' => __('Custom Shortcode Menu', 'bootstrap-basic'),
+    'popOut_primary' => __('Primary Pop-Out Shortcode Menu', 'bootstrap-basic'),
+    'popOut_secondary' => __('Secondary Pop-Out Shortcode Menu', 'bootstrap-basic'),
 ));
 
 // Function that will return our WordPress menu
